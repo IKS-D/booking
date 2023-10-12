64f33f862063f4832c741ec976003a8b9f38cd85
Mantelis <mantasjasikenas@gmail.com>
2023-10-12 15:09:09 +0300

    Add typescript types utilities

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,23 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// Helper utility to prettify types
+type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
+// Helper utility to combine two types
+type Combine<T1, T2> = Prettify<
+  {
+    [K in keyof (T1 | T2)]: T1[K] | T2[K];
+  } & Partial<T1 & T2>
+>;
+
+// Helper utility to make all properties mutable
+type Mutable<T> = {
+  -readonly [K in keyof T]: T[K];
+};
