cb81bbf2d703763d371e91d46f3192d6b05552f6
Mantelis <mantasjasikenas@gmail.com>
2023-10-17 18:28:45 +0300

    New logo for app Bump dependencies versions

--- components/BookingLogo.tsx
+++ components/BookingLogo.tsx
@@ -1,17 +1,18 @@
 import clsx from "clsx";
 import React from "react";
+import IksdLogo from "/public/iksd.svg";
 
 interface BookingLogoProps {
   className?: string;
 }
 
-// TODO Change BookingLogo component
 export default function BookingLogo({ className }: BookingLogoProps) {
   return (
     <div>
-      <p className={clsx("text-2xl font-bold text-primary", className)}>
+      {/* <p className={clsx("text-2xl font-bold text-primary", className)}>
         BOOKING
-      </p>
+      </p> */}
+      <IksdLogo className={clsx("w-10 h-10", className)} />
     </div>
   );
 }
--- components/ClientOnly.tsx
+++ components/ClientOnly.tsx
@@ -4,7 +4,6 @@ import { delay } from "framer-motion";
 import React, { useState, useEffect } from "react";
 import BookingLogo from "./BookingLogo";
 import { Spacer, Spinner } from "@nextui-org/react";
-import { title } from "@/components/primitives";
 
 interface ClientOnlyProps {
   children: React.ReactNode;
@@ -21,7 +20,7 @@ const ClientOnly: React.FC<ClientOnlyProps> = ({ children }) => {
     return (
       <div className="flex flex-col items-center justify-center h-[calc(100vh-1.5rem)]">
         <div className="">
-          <BookingLogo className={title()} />
+          <BookingLogo className={"w-40 h-40 animate-spin"} />
         </div>
         <Spacer y={5} />
         <Spinner size="md" />
