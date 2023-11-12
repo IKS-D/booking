1ac5bc8df94619a172a7eeee12a28fc8f5d75276
Mantelis <mantasjasikenas@gmail.com>
2023-11-12 13:54:06 +0200

    Update payment callback route with console.info and POST

--- app/payment/callback/route.ts
+++ app/payment/callback/route.ts
@@ -5,8 +5,20 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
-  console.log("Payment callback route");
-  console.log("request", request);
+  console.info("Payment callback route");
+  console.info("request", request);
+
+  return new Response("OK", {
+    headers: {
+      "content-type": "text/plain",
+    },
+    status: 200,
+  });
+}
+
+export async function POST(request: Request) {
+  console.info("Payment callback route");
+  console.info("request", request);
 
   return new Response("OK", {
     headers: {
