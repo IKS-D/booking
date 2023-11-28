c69e5effccf4763a080af76c33f206b371031f35
Mantelis <mantasjasikenas@gmail.com>
2023-11-28 13:53:29 +0200

    Refactor payment callback route to use URLSearchParams

--- app/payment/callback/route.ts
+++ app/payment/callback/route.ts
@@ -7,21 +7,19 @@ export async function POST(request: Request) {
   console.info("Payment callback route");
   console.info("request", request);
 
-  const requestUrl = new URL(request.url);
-  const data = requestUrl.searchParams.get("data");
-  const ss1 = requestUrl.searchParams.get("ss1");
-  const ss2 = requestUrl.searchParams.get("ss2");
+  const body = await request.text();
+  const params = new URLSearchParams(body);
+
+  const data = params.get("data");
+  const ss1 = params.get("ss1");
+  const ss2 = params.get("ss2");
 
   if (!data || !ss1 || !ss2) {
-    // get body from request
-    const body = await request.text();
-    console.info("body", body);
-
-    return new Response("OK", {
+    return new Response("Bad request", {
       headers: {
         "content-type": "text/plain",
       },
-      status: 200,
+      status: 400,
     });
   }
 
@@ -39,33 +37,3 @@ export async function POST(request: Request) {
     status: 200,
   });
 }
-
-// export async function GET(request: Request) {
-//   console.info("Payment callback route");
-//   console.info("request", request);
-
-//   const data = await request.json();
-
-//   const { error } = await insertPayment({
-//     amount: data.amount,
-//     date: new Date().toISOString(),
-//     first_name: data.name,
-//     last_name: data.surename,
-//     payer_email: data.p_email,
-//     payment_method: data.payment,
-//     payment_number: data.requestid,
-//     reservation_id: data.orderid,
-//     status: data.status,
-//   });
-
-//   if (error) {
-//     console.error(error);
-//   }
-
-//   return new Response("OK", {
-//     headers: {
-//       "content-type": "text/plain",
-//     },
-//     status: 200,
-//   });
-// }
