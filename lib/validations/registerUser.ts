369c786eab0a573f40cdaaffe50cbfb452ed9b74
lukvas <lukas.vasiliauskas@ktu.edu>
2023-12-08 16:30:21 +0200

    Fixed user registration validation, changed user registration to use react-hook-form.

--- lib/validations/registerUser.ts
+++ lib/validations/registerUser.ts
@@ -2,17 +2,13 @@ import { z } from "zod";
 
 export const UserRegistrationSchema = z
 	.object({
-		email: z.string().email(),
-		password: z.string().min(6, {
-			message: "Password must be at least 6 characters long.",
-		}),
-		confirm: z.string().min(6, {
-			message: "Password must be at least 6 characters long.",
-		}),
+		email: z.string().min(1, { message: "Email is required"}).email({ message: "Must be a valid email"}),
+		password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
+		confirmPassword: z.string().min(6, { message: "Password must be at least 6 characters long" }),
 	})
-	.refine((data) => data.confirm === data.password, {
-		message: "Passwords must match.",
-		path: ["confirm"],
+	.refine((data) => data.confirmPassword === data.password, {
+		message: "Passwords must match",
+		path: ["confirmPassword"],
 	});
 
 export type UserRegistrationFormData = z.infer<typeof UserRegistrationSchema>;
