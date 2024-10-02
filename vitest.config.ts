30afb25e64004211b0827bd1d4c98ee352e31537
Mantelis <mantasjasikenas@gmail.com>
2024-10-02 19:20:39 +0300

    Add database test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import { loadEnv } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   test: {
     environment: "jsdom",
     alias: {
       "@/": new URL("./", import.meta.url).pathname,
     },
+    env: loadEnv(mode, process.cwd(), ""),
   },
-});
+}));
