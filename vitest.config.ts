7d7b59154e699ae35719c5f791ff4fe54f8fd392
Mantelis <mantasjasikenas@gmail.com>
2024-10-11 12:52:54 +0300

    Update package scrips

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,14 @@ import { loadEnv } from "vite";
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   test: {
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*",
+      "**/playwright/**",
+    ],
     environment: "jsdom",
     alias: {
       "@/": new URL("./", import.meta.url).pathname,
