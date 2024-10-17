7394ffb258dbfe3073859206e39821591c9c7511
lukvas <lukas.vasiliauskas@ktu.edu>
2024-10-17 18:51:31 +0300

    Added a gui test for updating user's profile

---
+++ playwright/update-profile.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+
+test("Update profile", async ({ page }) => {
+  await page.goto("/profile");
+
+  await page.getByRole("link", { name: "Edit user profile" }).click();
+  await expect(page).toHaveURL("/profile/user/edit");
+
+  await page.locator('input[name="country"]').fill("Lithuania");
+
+  await page.getByRole("button", { name: "Save changes" }).click();
+  await expect(page).toHaveURL("/profile");
+});
